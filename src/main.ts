aa2239911abcb98796477e9a98c3cc60fe4d8489
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-22 18:53:14 +0000

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,20 +18,35 @@ if (ctx) {
 }
 
 class Particle {
-  constructor() {
+  x: number;
+  y: number;
+
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
 
   }
   update() {
 
   }
   draw() {
-    
+
   }
 }
 
 class Effect {
+  width: number;
+  height: number;
+  particles: [];
+
     constructor(width, height) {
-      
+      this.width = width;
+      this.height = height;
+      this.particles = []
+    }
+    init() {
+      this.particles.push(new Particle());
     }
 }
 
