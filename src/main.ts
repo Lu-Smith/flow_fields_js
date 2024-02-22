0b007d589b0f0a6befb3490b24b8fa42f6f5029a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-22 18:38:50 +0000

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-import './style.css'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <canvas id="canvas1"></canvas>
-    <footer>Coded by
-    <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>
-    -
-    <a href="https://github.com/Lu-Smith/flow_fields_js" target="_blank">GitHub</a></footer>
-  </div>
-`
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-
+console.log(ctx);
