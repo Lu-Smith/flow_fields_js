03d27491bfdcdc01d4e2f4420eec0f012f4b8f5c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-22 18:47:58 +0000

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import './style.css';
 
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas: HTMLCanvasElement = document.getElementById('canvas1') as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-console.log(ctx);
+if (ctx) {
+  //canvas settings
+  ctx.fillStyle = 'white';
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 10;
+  ctx.lineCap = 'round';
+  ctx.beginPath();
+  ctx.moveTo(100,200);
+  ctx.lineTo(400, 500);
+  ctx.stroke();
+}
+
+class Particle {
+  constructor() {
+
+  }
+  update() {
+
+  }
+  draw() {
+    
+  }
+}
+
+class Effect {
+    constructor(width, height) {
+      
+    }
+}
+
