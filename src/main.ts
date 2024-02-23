4c64fa74ef79556a9d05eb70818b0b711c44d63a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-23 16:00:21 +0000

    Updated Footer style

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css';
 const canvas: HTMLCanvasElement = document.getElementById('canvas1') as HTMLCanvasElement;
 const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight * 0.94;
+canvas.height = window.innerHeight;
 
 if (ctx) {
   //canvas settings
