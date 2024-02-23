ffd309efbeae33a7b2a215fb97f2cffe30efba6f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-23 13:48:46 +0000

    Updated maint.ts

--- src/main.ts
+++ src/main.ts
@@ -22,32 +22,39 @@ class Particle {
   history: [{x: number, y: number}];
   maxLength: number;
   angle: number;
+  speedModifier: number;
+  timer: number;
 
   constructor(effect: Effect) {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
-    this.speedX = Math.random() * 5 - 2.5;
-    this.speedY = Math.random() * 5 - 2.5;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.speedModifier = Math.floor(Math.random() * 5 + 1);
     this.history = [{x: this.x, y: this.y}];
     this.maxLength = Math.floor(Math.random() * 200 + 10);
     this.angle = 0;
+    this.timer = this.maxLength * 2;
   }
   update() {
-    let x = Math.floor(this.x / this.effect.cellSize);
-    let y = Math.floor(this.y / this.effect.cellSize);
-    let index = y * this.effect.cols + x;
-    this.angle = this.effect.flowField[index];
-
-    this.speedX = Math.cos(this.angle);
-    this.speedY = Math.sin(this.angle);
-    this.x += this.speedX;
-    this.y += this.speedY;
-
-    this.history.push({x: this.x, y: this.y});
-    if( this.history.length > this.maxLength) {
-      this.history.shift();
-    };
+    this.timer--;
+    if (this.timer >= 1) {
+      let x = Math.floor(this.x / this.effect.cellSize);
+      let y = Math.floor(this.y / this.effect.cellSize);
+      let index = y * this.effect.cols + x;
+      this.angle = this.effect.flowField[index];
+  
+      this.speedX = Math.cos(this.angle);
+      this.speedY = Math.sin(this.angle);
+      this.x += this.speedX * this.speedModifier;
+      this.y += this.speedY * this.speedModifier;
+  
+      this.history.push({x: this.x, y: this.y});
+      if( this.history.length > this.maxLength) {
+        this.history.shift();
+      };
+    }
   }
   draw(context: CanvasRenderingContext2D) {
     context.beginPath();
@@ -56,6 +63,11 @@ class Particle {
       context.lineTo(this.history[i].x, this.history[i].y);
     };
     context.stroke();
+  }
+  reset() {
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+    this.history = [{x: this.x, y: this.y}];
   }
 }
 
@@ -81,7 +93,7 @@ class Effect {
       this.cols = 0;
       this.flowField = [];
       this.curve = 0.5;
-      this.zoom = 0.2;
+      this.zoom = 0.13;
       this.init();
     }
     init() {
